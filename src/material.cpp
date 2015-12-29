b6defb6896c2ab9547e30b80473c96848e006351
n-p <naim.c.sharp@gmail.com>
2015-12-29 15:22:44 +0330

    to measure 1 cp elo (dataset size is 6500)

--- src/material.cpp
+++ src/material.cpp
@@ -31,28 +31,28 @@ namespace {
   // Polynomial material imbalance parameters
 
   //                      pair  pawn knight bishop rook queen
-  const int Linear[6] = { 1667, -168, -1027, -166,  238, -138 };
+  const int Linear[6] = { 1681, -178, -1026, -154,  243, -132 };
 
   const int QuadraticOurs[][PIECE_TYPE_NB] = {
     //            OUR PIECES
     // pair pawn knight bishop rook queen
-    {   0                               }, // Bishop pair
-    {  40,    2                         }, // Pawn
-    {  32,  255,  -3                    }, // Knight      OUR PIECES
-    {   0,  104,   4,    0              }, // Bishop
-    { -26,   -2,  47,   105,  -149      }, // Rook
-    {-185,   24, 122,   137,  -134,   0 }  // Queen
+    {   -29                               }, // Bishop pair
+    {  40,    22                         }, // Pawn
+    {  68,  255,  20                    }, // Knight      OUR PIECES
+    {   0,  84,   4,    0              }, // Bishop
+    { -29,   -2,  19,   105,  -137      }, // Rook
+    {-185,   62, 106,   137,  -134,   -18 }  // Queen
   };
 
   const int QuadraticTheirs[][PIECE_TYPE_NB] = {
     //           THEIR PIECES
     // pair pawn knight bishop rook queen
     {   0                               }, // Bishop pair
-    {  36,    0                         }, // Pawn
-    {   9,   63,   0                    }, // Knight      OUR PIECES
-    {  59,   65,  42,     0             }, // Bishop
-    {  46,   39,  24,   -24,    0       }, // Rook
-    { 101,  100, -37,   141,  268,    0 }  // Queen
+    {  37,    0                         }, // Pawn
+    {   39,   58,   0                    }, // Knight      OUR PIECES
+    {  35,   65,  42,     0             }, // Bishop
+    {  59,   40,  24,   -28,    0       }, // Rook
+    { 85,  85, -37,   122,  264,    0 }  // Queen
   };
 
   // Endgame evaluation and scaling functions are accessed directly and not through
